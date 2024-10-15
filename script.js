5f6e821a3b3775e3cb076c27047791320616ce4a
Faustina-Acheampong <157317178+Faustina-Acheampong@users.noreply.github.com>
2024-10-15 16:44:51 +0200

    Handle dice roll with async/await.

--- script.js
+++ script.js
@@ -40,22 +40,14 @@ const fetchJokeById = async () => {
             console.log(result);
 
             const response = await fetch('https://official-joke-api.appspot.com/jokes/random');
+            const data = response.json();
+            const joke = `${data.setup} \n ${data.punchline}`;
+        document.getElementById('jokeDisplay').textContent = `Yay, you rolled a six! Here's a joke: ${joke}`;
 
-    .then((response) => response.json()) 
-    .then((data) => {
-        const joke = `${data.setup} \n ${data.punchline}`;
-        document.getElementById('jokeDisplay').textContent = `Yay, you got a six! Here's a joke: ${joke}`;
-        })
-
-        .catch((error) => {
-            console.error('Error fetching a joke:', error);
-            document.getElementById('jokeDisplay').textContent = 'Failed to fetch joke.';
-        });
-    } else {
-        console.log('Try again! No joke this time.');
-        document.getElementById('jokeDisplay').textContent = 'Try again! No joke this time.';
-
-    }
+    } catch (error) {
+            console.error(error);
+            document.getElementById('jokeDisplay').textContent = error;
+        }
 };
 
 document.getElementById('fetchJokeButton').addEventListener('click', fetchJokeById);
