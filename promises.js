adba3dc7673d685c43ca0c7003d02e85c83d4264
Faustina-Acheampong <157317178+Faustina-Acheampong@users.noreply.github.com>
2024-10-15 05:04:39 +0200

    Create function to fetch advice from API

--- promises.js
+++ promises.js
@@ -22,11 +22,19 @@ checkNumber(1002)
 const fetchAdviceById = (id) => {
     fetch(`https://api.adviceslip.com/advice/${id}`)
    .then((response) => {
-    console.log(response);
+    //console.log(response);
+    if (!response.ok) {
+      throw new Error('Failed to fetch advice');
+   }
+    return response.json();
+})
+   .then((data) => {
+    const advice = data.slip.advice;
+    console.log(`Advice (ID: ${id}): ${advice}`);      
+   })
+   .catch((error) => {
+    console.error('Error fetching advice:', error); 
    });
+}
 
-};
-
-
-
-fetchAdviceById(185);
+fetchAdviceById(15);
